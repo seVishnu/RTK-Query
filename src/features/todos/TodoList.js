f16b6c841241b56325d3c5d5d6d5c8d8643ebd10
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-26 18:09:11 +0530

    Initial commit

---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,8 @@
+import { faUpload, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+const TodoList = () => {
+  const [todo, setTodo] = useState("");
+  return <div>TodoList</div>;
+};
+
+export default TodoList;
