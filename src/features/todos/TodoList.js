7a983bef77566b60a69806efae4250eeddef1ae7
vishnuSekhar <vishnusekhar96@gmail.com>
2022-12-27 18:31:17 +0530

    Complete

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,8 +1,75 @@
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useDeleteTodoMutation,
+  useUpdateTodoMutation,
+} from "../api/apiSlice";
 import { faUpload, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 const TodoList = () => {
   const [todo, setTodo] = useState("");
-  return <div>TodoList</div>;
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({ userId: 1, title: todo, completed: false });
+    setTodo("");
+  };
+  const newItemSection = (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="new-todo">Enter a new Todo Item</label>
+      <div className="new-todo">
+        <input
+          id="new-todo"
+          name="new-todo"
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="enter new todo"
+        />
+      </div>
+      <button className="submit">
+        <FontAwesomeIcon icon={faUpload} />
+      </button>
+    </form>
+  );
+  let content;
+  if (isLoading) content = <p>Loading...</p>;
+  else if (isSuccess) {
+    content = todos.map((todo) => (
+      <article key={todo.id}>
+        <div className="todo">
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            id={todo.id}
+            onChange={() => updateTodo({ ...todo, completed: !todo.completed })}
+          />
+          <label htmlFor={todo.id}>{todo.title}</label>
+        </div>
+        <button className="trash" onClick={() => deleteTodo({ id: todo.id })}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+      </article>
+    ));
+  } else if (isError) content = <p>{error}</p>;
+
+  return (
+    <main>
+      <h1>Todo List</h1>
+      {newItemSection}
+      {content}
+    </main>
+  );
 };
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApiProvider api={apiSlice}>
+    <App />
+  </ApiProvider>
+);
